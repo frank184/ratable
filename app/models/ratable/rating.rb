f13c29546f3ce5067e7abc55304449c9ce9cb76d
frank184 <francoisbelanger1993@hotmail.com>
2016-04-18 20:21:05 -0400

    added scopes for ratings

--- app/models/ratable/rating.rb
+++ app/models/ratable/rating.rb
@@ -2,5 +2,8 @@ module Ratable
   class Rating < ActiveRecord::Base
     belongs_to :ratee, polymorphic: true
     belongs_to :rater, polymorphic: true
+
+    scope :by_ratee, -> (ratee) { where(ratee: ratee) }
+    scope :by_rater, -> (rater) { where(rater: rater) }
   end
 end
